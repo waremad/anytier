a049472453534fd89ae4940fd1425c38fd917f99
waremad <mycarline5@gmail.com>
2025-05-03 10:54:01 +0900

    初めに

--- anytier.py
+++ anytier.py
@@ -1,1 +1,17 @@
 #
+"""
+A---A----A
+B-|   |
+C---C-|
+D-|
+
+B-|
+D---D
+
+"""
+ls = {"A":[],"B":["A","D"],"C":["A"],"D":["C"]}
+
+def make_outdict(dic):
+    out = {}
+    for i in list(dic.keys()):
+        
